f511249ea7a3287dfa80f0305c8f0d4366108d4b
Hung Neo <hung.nguyen@digia.com>
2017-09-18 17:41:49 +0300

    Added test cases

---
+++ tests/_support/AuthorRule.php
@@ -0,0 +1,19 @@
+<?php
+
+use Crisu83\Overseer\Entity\Resource;
+use Crisu83\Overseer\Entity\Rule;
+use Crisu83\Overseer\Entity\Subject;
+
+class AuthorRule implements Rule
+{
+    /**
+     * @inheritdoc
+     */
+    public function evaluate(Subject $subject, Resource $resource, array $params)
+    {
+        if (!$resource instanceof Book) {
+            return false;
+        }
+        return $resource->getAuthorId() === $subject->getSubjectId();
+    }
+}
---
+++ tests/_support/User.php
@@ -0,0 +1,45 @@
+<?php
+
+use Crisu83\Overseer\Entity\Subject;
+
+class User implements Subject
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * User constructor.
+     *
+     * @param int $id
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSubjectId()
+    {
+        return (string)$this->getId();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSubjectName()
+    {
+        return 'user';
+    }
+}
